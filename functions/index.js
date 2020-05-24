1691152d59aef61ff093efd65437072f805a18e7
Anudeep Chandra Paul <achandrapaul@manh.com>
2020-05-24 16:22:31 +0530

    Adding Dynamc Response

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,14 @@
 const functions = require("firebase-functions");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get("/getAppProperties", (req, res) => {
   res.statusCode = 200;
@@ -33,36 +36,56 @@ app.get("/getAppProperties", (req, res) => {
   });
 });
 
-
-app.get('/modules', (req,res) => {
+app.get("/modules", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   return res.json({
-    title: 'Modules'
-    , modules: {
-      text: 'All Modules'
-      , url: '/modules'
-      , scheme: '/modules'
-      , list: [
-        { text: "Python", url: "/python", scheme: '/[moduleId]' },
-        { text: "SQL", url: "/sql", scheme: '/[moduleId]' },
-        { text: "Math", url: "/math" , scheme: '/[moduleId]'},
-        { text: "Statistics", url: "/statistics", scheme: '/[moduleId]' },
-        { text: "AI & ML 1", url: "/ai-ml-1", scheme: '/[moduleId]' }
-      ]
-    }
-  })
-})
+    title: "Modules",
+    modules: {
+      text: "All Modules",
+      url: "/modules",
+      scheme: "/modules",
+      list: [
+        { text: "Python", url: "/python", scheme: "/[moduleId]" },
+        { text: "SQL", url: "/sql", scheme: "/[moduleId]" },
+        { text: "Math", url: "/math", scheme: "/[moduleId]" },
+        { text: "Statistics", url: "/statistics", scheme: "/[moduleId]" },
+        { text: "AI & ML 1", url: "/ai-ml-1", scheme: "/[moduleId]" },
+      ],
+    },
+  });
+});
 
-app.get('/modules/:moduleId', (req,res) => {
+const getModuleName = (id) => {
+  switch (id.toLowerCase()) {
+    case "python":
+      return { text: "Python", title: "Python Learning Modules" };
+    case "sql":
+      return { text: "SQL", title: "SQL Learning Modules" };
+    case "math":
+      return { text: "Mathematics", title: "Mathematics Learning Modules" };
+    case "statistics":
+      return { text: "Statistics", title: "Statistics Learning Modules" };
+    case "ai-ml-1":
+      return {
+        text: "Artificial & Maching Learning (I)",
+        title: "Artificial & Maching Learning (I) Learning Modules",
+      };
+    default:
+      return { text: id, title: "Unknown Module" };
+  }
+};
+
+app.get("/modules/:moduleId", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  const data = getModuleName(req.params.moduleId);
   return res.json({
-    title: 'Modules'
-    , modules: {
-      text: 'Python'
-      , url: '/python'
-      , scheme: '/[moduleId]'
-    }
-  })
-})
+    title: data.title,
+    modules: {
+      text: data.text,
+      url: `/${req.params.moduleId}`,
+      scheme: "/[moduleId]",
+    },
+  });
+});
 
 exports.api = functions.https.onRequest(app);
