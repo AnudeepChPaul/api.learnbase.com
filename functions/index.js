4f7e56c02b0692f361bff4c30fbb0889c92017cf
Anudeep Chandra Paul <achandrapaul@manh.com>
2020-05-25 19:45:24 +0530

    Adding segregated api changes

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,6 @@ const bodyParser = require("body-parser");
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-
 const app = express();
 
 app.use(bodyParser.json());
@@ -15,44 +14,66 @@ app.get("/getAppProperties", (req, res) => {
   res.setHeader("Content-Type", "application/json");
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.json({
-    title: "Create Next App",
-    header: {
-      title: "Home",
-      url: "/",
-      modules: {
-        text: "All Modules",
-        url: "/modules",
-        scheme: "/modules",
-        list: [
-          { text: "Python", url: "/python", scheme: "/[moduleId]" },
-          { text: "SQL", url: "/sql", scheme: "/[moduleId]" },
-          { text: "Math", url: "/math", scheme: "/[moduleId]" },
-          { text: "Statistics", url: "/statistics", scheme: "/[moduleId]" },
-          { text: "AI & ML 1", url: "/ai-ml-1", scheme: "/[moduleId]" },
-        ],
-      },
-      buttons: [{ text: "About", url: "/about", scheme: "/about" }],
+    title: "Learn",
+    allModules: {
+      text: "All Modules",
+      url: "/modules",
+      scheme: "/modules",
     },
   });
 });
 
-app.get("/modules", (req, res) => {
+app.get("/modules/list", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  return res.json({
+  const listData = {
     title: "Modules",
     modules: {
-      text: "All Modules",
-      url: "/modules",
-      scheme: "/modules",
+      baseUrl: "/modules",
       list: [
         { text: "Python", url: "/python", scheme: "/[moduleId]" },
         { text: "SQL", url: "/sql", scheme: "/[moduleId]" },
         { text: "Math", url: "/math", scheme: "/[moduleId]" },
         { text: "Statistics", url: "/statistics", scheme: "/[moduleId]" },
         { text: "AI & ML 1", url: "/ai-ml-1", scheme: "/[moduleId]" },
+        { text: "AI & ML 2", url: "/ai-ml-2", scheme: "/[moduleId]" },
+        {
+          text: "Linear Regression",
+          url: "/linear-regression",
+          scheme: "/[moduleId]",
+        },
+        {
+          text: "Simple Linear Regression",
+          url: "/simple-linear-regression",
+          scheme: "/[moduleId]",
+        },
       ],
     },
-  });
+  };
+
+  const { top } = req.query;
+  const listDataTop5 = {
+    title: "Modules",
+    modules: {
+      baseUrl: "/modules",
+      list: [
+        { text: "Python", url: "/python", scheme: "/[moduleId]" },
+        { text: "AI & ML 1", url: "/ai-ml-1", scheme: "/[moduleId]" },
+        { text: "AI & ML 2", url: "/ai-ml-2", scheme: "/[moduleId]" },
+        {
+          text: "Linear Regression",
+          url: "/linear-regression",
+          scheme: "/[moduleId]",
+        },
+        {
+          text: "Simple Linear Regression",
+          url: "/simple-linear-regression",
+          scheme: "/[moduleId]",
+        },
+      ],
+    },
+  };
+
+  return top ? res.json(listDataTop5) : res.json(listData);
 });
 
 const getModuleName = (id) => {
@@ -69,6 +90,21 @@ const getModuleName = (id) => {
       return {
         text: "Artificial & Maching Learning (I)",
         title: "Artificial & Maching Learning (I) Learning Modules",
+      };
+    case "ai-ml-2":
+      return {
+        text: "Artificial & Maching Learning (II)",
+        title: "Artificial & Maching Learning (II) Learning Modules",
+      };
+    case "linear-regression":
+      return {
+        text: "Linear Regression",
+        title: "Linear Regression Learning Modules",
+      };
+    case "simple-linear-regression":
+      return {
+        text: "Simple Linear Regression",
+        title: "Simple Linear Regression Learning Modules",
       };
     default:
       return { text: id, title: "Unknown Module" };
